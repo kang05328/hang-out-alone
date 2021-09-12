beb94695a37cd96f5676a4497a334fa3917d383a
kang05328 <kang05328@gmail.com>
2021-09-12 15:52:58 +0900

    setting member path

---
+++ src/main/java/com/hang/out/alone/controller/DataController.java
@@ -0,0 +1,7 @@
+package com.hang.out.alone.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DataController {
+}
---
+++ src/main/java/com/hang/out/alone/dto/Member.java
@@ -0,0 +1,13 @@
+package com.hang.out.alone.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private String memberId;
+    private String password;
+    private String address;
+    private String memberType;
+}
