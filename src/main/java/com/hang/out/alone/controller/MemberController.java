f9317db58b07e7947a155345240fd9844243481b
bamiar <ginea00@gmail.com>
2021-09-23 21:39:04 +0900

    mybatis mapper test

--- src/main/java/com/hang/out/alone/controller/MemberController.java
+++ src/main/java/com/hang/out/alone/controller/MemberController.java
@@ -1,17 +1,47 @@
 package com.hang.out.alone.controller;
 
+import com.hang.out.alone.dto.Member;
 import com.hang.out.alone.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-//Controller, RestController 차이점 확인
-@Controller("/member")
+
+//@RestController("/account") API 설계 확정후 사용여부 결정
 public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     //TODO : 회원 가입
-    @RequestMapping("/sign-in")
-    public void signIn() {
-
+    @PostMapping("/account")
+    public void signIn(String id, String pw, String address, int memType) {
+//        Member member = new Member();
+//        member.setAccount(id);
+//        member.setPassword(pw);
+//        member.setAddress(address);
+//        member.setMemberType(memType);
+//        memberService.createMember(member);
     }
+
+//    @GetMapping("/account")
+//    public void logIn() {
+//
+//    }
+//
+//    @DeleteMapping("/account")
+//    public void delete() {
+//
+//    }
+
+    //TODO 회원정보 조회
+    @GetMapping("/user")
+    public Member getMember() {
+        return memberService.getMember();
+    }
+
+//    @PostMapping
+//    public Member updateMember() {
+//    }
+
 }
--- src/main/java/com/hang/out/alone/service/MemberService.java
+++ src/main/java/com/hang/out/alone/service/MemberService.java
@@ -1,8 +1,51 @@
 package com.hang.out.alone.service;
 
+import com.hang.out.alone.dto.Member;
+import com.hang.out.alone.mapper.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+    @Autowired
+    private MemberMapper mapper;
+
+    public void createMember(Member member) {
+
+    }
+
+    //예외처리 메소드들
+    private boolean isValidId() {
+        return false;
+    }
+
+    private boolean isValidPw() {
+        return false;
+    }
+
+    private boolean isValidAddress() {
+        return false;
+    }
+
+    private boolean isValidType() {
+        return false;
+    }
+
+    public Member getMember() {
+        return mapper.getAccount();
+    }
+
+    public Member insertMember() {
+        return mapper.createAccount();
+    }
+
+    private void updateMember() {
+
+    }
+
+    private void deleteMember() {
+
+    }
+
 
 }
