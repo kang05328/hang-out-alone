dd9dcd35a175f3b0efe20f120f82b7db0326189c
kang05328 <kang05328@gmail.com>
2021-09-26 13:14:07 +0900

    use open api data instead of test data

--- src/main/java/com/hang/out/alone/controller/DataController.java
+++ src/main/java/com/hang/out/alone/controller/DataController.java
@@ -6,14 +6,14 @@ import com.hang.out.alone.service.ESService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.*;
-
 @Controller
 public class DataController {
 
     private final DataService dataService;
     private final ESService esService;
     private final ObjectMapper objectMapper;
+    private final String CULTURE_PLACE = "culture_place";
+    private final String CULTURE_EVENT = "culture_event";
 
     public DataController(final DataService dataService, final ESService esService, final ObjectMapper objectMapper) {
         this.dataService = dataService;
@@ -21,21 +21,24 @@ public class DataController {
         this.objectMapper = objectMapper;
     }
 
+    // TODO : POST 로 변경
     @RequestMapping("/culture/place/insert")
-    public String test() {
-        // TODO : 메서드 명 변경 test -> addDocument
-        // TODO : 테스트 데이터 실제 데이터로 변경작업
+    public String insertPlace() {
         try {
-            //String testData = "{\"FAC_CODE\":\"101124\",\"SUBJCODE\":1.0,\"CODENAME\":\"공연장\",\"FAC_NAME\":\"신도림 오페라하우스\",\"ADDR\":\"서울시 구로구 구로동 3-39\"}";
-            //CulturePlace culturePlace = objectMapper.readValue(Objects.requireNonNull(testData), new TypeReference<CulturePlace>() {
-            //});
-            //CulturePlace culturePlace = new CulturePlace("101124", "1.0", "공연장", "신도림 오페라하우스", "서울시 구로구 구로동 3-39");
-            Map<String, Object> jsonMap = new HashMap<>();
-            jsonMap.put("test", "test2");
-            jsonMap.put("postDate", new Date());
-            jsonMap.put("message", "testtest2");
+            String jsonString = dataService.culturePlace();
+            esService.addDocument(jsonString, CULTURE_PLACE);
+            return "index";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-            esService.addDocument(jsonMap);
+    // TODO : POST 로 변경
+    @RequestMapping("/culture/event/insert")
+    public String insertEvent() {
+        try {
+            String jsonString = dataService.cultureEvent();
+            esService.addDocument(jsonString, CULTURE_PLACE);
             return "index";
         } catch (Exception e) {
             throw new RuntimeException(e);
--- src/main/java/com/hang/out/alone/service/ESService.java
+++ src/main/java/com/hang/out/alone/service/ESService.java
@@ -8,29 +8,23 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
 @Log4j2
 public class ESService {
 
     private final RestHighLevelClient restHighLevelClient;
-    private final DataService dataService;
 
-    public ESService(final RestHighLevelClient restHighLevelClient, final DataService dataService) {
+    public ESService(final RestHighLevelClient restHighLevelClient) {
         this.restHighLevelClient = restHighLevelClient;
-        this.dataService = dataService;
     }
 
     // TODO : create index, get document, update document, find 등 구현 필요.
 
-    public void addDocument(Map<String, Object> objectMap) {
+    public void addDocument(String jsonString, String indexName) {
         try {
             // TODO : index 이름 변경
 
-            String jsonString = dataService.culturePlace();
-
-            IndexRequest indexRequest = new IndexRequest("event_place").id("1")
+            IndexRequest indexRequest = new IndexRequest(indexName)
                     .source(jsonString, XContentType.JSON);
 
             IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
