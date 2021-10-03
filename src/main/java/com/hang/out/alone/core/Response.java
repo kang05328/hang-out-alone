5d6a481f104f0e94738dce0ef72c00eee460e464
bamiar <ginea00@gmail.com>
2021-10-03 20:51:49 +0900

    add Response format add Cloud SQL as Postgres

--- src/main/java/com/hang/out/alone/core/Response.java
+++ src/main/java/com/hang/out/alone/core/Response.java
@@ -1,4 +1,12 @@
 package com.hang.out.alone.core;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Response {
+    private boolean success;
+    private String result;
+    private String message;
 }
